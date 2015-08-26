c2488c98eabb176d6e8fce3dc9e8d41da141e0fb
Mr. Nichols <mrnichol@garlandisd.net>
2015-08-26 15:46:27 -0500

    Added tests and TODOs.

--- src/cs3/lists/SingleStringList.java
+++ src/cs3/lists/SingleStringList.java
@@ -8,41 +8,49 @@ public class SingleStringList implements StringList {
 
     @Override
     public int size() {
+	// TODO Write the size method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public String get(int index) {
+	// TODO Write the get method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void add(String element) {
+	// TODO Write the add(String) method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void add(int index, String element) {
+	// TODO Write the add(int,String) method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void remove(int index) {
+	// TODO Write the remove method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public int indexOf(String element) {
+	// TODO Write the indexOf method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean contains(String element) {
+	// TODO Write the contains method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public String[] toArray() {
+	// TODO Write the toArray method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
 
@@ -55,6 +63,7 @@ public class SingleStringList implements StringList {
      */
     @Override
     public String toString() {
+	// TODO Write the toString method
 	throw new UnsupportedOperationException("Not supported yet.");
     }
     
--- test/cs3/lists/SingleStringListTest.java
+++ test/cs3/lists/SingleStringListTest.java
@@ -1,11 +1,7 @@
 package cs3.lists;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,6 +10,7 @@ import static org.junit.Assert.*;
  * @author Mr. Nichols
  */
 public class SingleStringListTest {
+    private static final String[] strs = new String[] { "aardvark", "zebra", "splat" };
     private StringList sl;
     
     public SingleStringListTest() {
@@ -24,10 +21,6 @@ public class SingleStringListTest {
 	sl = new SingleStringList();
     }
     
-    @After
-    public void tearDown() {
-    }
-
     /**
      * A list should be empty to start with.
      */
@@ -85,8 +78,10 @@ public class SingleStringListTest {
 	    assertEquals(strs[i], sl.get(i));
 	}
     }
-    private static final String[] strs = new String[] { "aardvark", "zebra", "splat" };
 
+    /**
+     * Adding to beginning of the list.
+     */
     @Test
     public void testAddBeginning() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -97,6 +92,9 @@ public class SingleStringListTest {
 	assertEquals("splat", sl.get(3));
     }
 
+    /**
+     * Adding to the middle of the list.
+     */
     @Test
     public void testAddMiddle() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -107,6 +105,9 @@ public class SingleStringListTest {
 	assertEquals("splat", sl.get(3));
     }
 
+    /**
+     * Using add(int,String) to add to the end of the list.
+     */
     @Test
     public void testAddEnd() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -117,6 +118,9 @@ public class SingleStringListTest {
 	assertEquals("woof", sl.get(3));
     }
 
+    /**
+     * Adding just before the last element.
+     */
     @Test
     public void testAddNextToEnd() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -127,11 +131,17 @@ public class SingleStringListTest {
 	assertEquals("woof", sl.get(2));
     }
 
+    /**
+     * Attempting to remove from an empty list.
+     */
     @Test(expected=IndexOutOfBoundsException.class)
     public void testRemoveNothing() {
 	sl.remove(0);
     }
 
+    /**
+     * Test removal from the middle of the list.
+     */
     @Test
     public void testRemove() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -141,6 +151,9 @@ public class SingleStringListTest {
 	assertEquals("splat",sl.get(1));
     }
 
+    /**
+     * Test removal of a single element from a singleton list.
+     */
     @Test
     public void testRemove2() {
 	sl.add("foo");
@@ -149,6 +162,51 @@ public class SingleStringListTest {
 	assertEquals(0,sl.size());
     }
 
+    /**
+     * Test removal of the first element of a list.
+     */
+    @Test
+    public void testRemove3() {
+	Arrays.stream(strs).forEach(sl::add);
+	sl.remove(0);
+	assertEquals(2,sl.size());
+	assertEquals("zebra",sl.get(0));
+	assertEquals("splat",sl.get(1));
+    }
+
+    /**
+     * Test removal of the last element of a list.
+     */
+    @Test
+    public void testRemove4() {
+	Arrays.stream(strs).forEach(sl::add);
+	sl.remove(2);
+	assertEquals(2,sl.size());
+	assertEquals("aardvark",sl.get(0));
+	assertEquals("zebra",sl.get(1));
+    }
+
+    /**
+     * Test invalid removal operation.
+     */
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testBadRemove() {
+	Arrays.stream(strs).forEach(sl::add);
+	sl.remove(3);
+    }
+
+    /**
+     * Test invalid removal operation.
+     */
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testBadRemove2() {
+	Arrays.stream(strs).forEach(sl::add);
+	sl.remove(-1);
+    }
+
+    /**
+     * Test indexOf method.
+     */
     @Test
     public void testIndexOf() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -158,11 +216,17 @@ public class SingleStringListTest {
 	assertEquals(-1,sl.indexOf("porcupine"));
     }
 
+    /**
+     * Test indexOf on empty list.
+     */
     @Test
     public void testIndexOf2() {
 	assertEquals(-1,"foo");
     }
 
+    /**
+     * Test contains method.
+     */
     @Test
     public void testContains() {
 	Arrays.stream(strs).forEach(sl::add);
@@ -172,34 +236,52 @@ public class SingleStringListTest {
 	assertFalse(sl.contains("computer"));
     }
 
+    /**
+     * Test of toArray method.
+     */
     @Test
     public void testToArray() {
 	Arrays.stream(strs).forEach(sl::add);
 	assertArrayEquals(strs, sl.toArray());
     }
 
+    /**
+     * Test of toString method.
+     */
     @Test
     public void testToString() {
 	Arrays.stream(strs).forEach(sl::add);
 	assertEquals("[aardvark, zebra, splat]", sl.toString());
     }
 
+    /**
+     * Null check test on add(String).
+     */
     @Test(expected=NullPointerException.class)
     public void testNullCheck1() {
 	sl.add(null);
     }
 
+    /**
+     * Null check test on add(int,String).
+     */
     @Test(expected=NullPointerException.class)
     public void testNullCheck2() {
 	sl.add(0, null);
     }
 
+    /**
+     * Null check test on indexOf.
+     */
     @Test(expected=NullPointerException.class)
     public void testNullCheck3() {
 	sl.add("foo");
 	sl.indexOf(null);
     }
 
+    /**
+     * Null check test on contains.
+     */
     @Test(expected=NullPointerException.class)
     public void testNullCheck4() {
 	sl.add("foo");
